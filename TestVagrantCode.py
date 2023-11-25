aefe19c13411e089cd841053a0623c384be69705
Varunkumareddy123 <151989381+Varunkumareddy123@users.noreply.github.com>
2023-11-25 13:55:11 +0530

    Add files via upload

---
+++ TestVagrantCode.py
@@ -0,0 +1,28 @@
+name:dubbireddy gari varun kumar reddy
+college:bmsit
+year of passing:2024
+phone number:9391737847
+email id:dubbireddygarivarun@gmail.com
+
+import pandas as pd
+from math import matlib
+def calculate_gst(unit_rate,gst_rate,quantity):
+    return(unit_rate,gst_rate,quantity)
+
+def calculate_total_amount(unit_rate,gst_rate,quantity):
+    return(rate+gst_amount)+quantity
+
+object=[things:'leather wallet','umbrella','cigarette','honey'
+        unit_rate:1100,900,200,100
+        gst_rate:18,12,28,0
+        quntity:1,4,3,2]
+
+max_gst_rate=0
+total_amount=0
+
+
+
+
+
+
+
